57f4151252e8b184dd08260f6d95ba060e0151f5
Miao-Long <myau.christopher@gmail.com>
2022-11-15 15:16:59 -0800

    Added density mod

--- sampling.py
+++ sampling.py
@@ -20,17 +20,19 @@ def csv_reader(directory_path:str) -> dict:
             except Exception as e: print(e)
     return [df_dict, x/c]
 
-def weighted_sample(d:dict, weight:float, random_state:int=None):
+def weighted_sample(d:dict, sample_size:int, avg_len:int, random_state:int=None):
     df_list = []
     for key in d.keys():
-        df_list.append(d[key].sample(int(len(d[key]) * weight),random_state=random_state))
+        print(float(len(d[key]) / avg_len) * float(len(d[key]) / avg_len) * sample_size )
+        df_list.append(d[key].sample(int(float(len(d[key]) / avg_len) * float(len(d[key]) / avg_len) * sample_size) ,random_state=random_state))
     return pd.concat(df_list)
 
-def execute(directory_path: str, export_path: str, sample_size: int=100, runs: int=1, random_state:int=None):
+def execute(directory_path: str, export_path: str, sample_size: int=100, runs: int=1, random_state:int=None, write=True):
     [data_full, avg_len] = csv_reader(directory_path)
     for i in range(runs):
-        data_sampled_combined = weighted_sample(data_full, float(sample_size/avg_len), random_state)
-        data_sampled_combined.to_csv(f"{export_path}/UkraineWarSampled_{i}.csv")
+        data_sampled_combined = weighted_sample(data_full, sample_size, avg_len, random_state)
+        if write:
+            data_sampled_combined.to_csv(f"{export_path}/UkraineWarSampled_{i}.csv")
 
 if __name__ == "__main__":
     directory_path = sys.argv[1]
@@ -38,7 +40,7 @@ if __name__ == "__main__":
     sample_size = sys.argv[3]
     runs = sys.argv[4]
 
-    execute(directory_path, export_path, int(sample_size), int(runs))
+    execute(directory_path, export_path, int(sample_size), int(runs), write=False)
 
 
 
