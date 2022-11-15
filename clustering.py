04471f9dc13ac76fc6d5e3a4e37fd4f97c2403d5
Miao-Long <myau.christopher@gmail.com>
2022-11-15 01:27:30 -0800

    fleshed out serialization

---
+++ clustering.py
@@ -0,0 +1,8 @@
+import csv
+import os
+import sklearn as sks
+import sys
+import pandas as pd
+
+def cluster():
+    print("cluster")
--- serialization.py
+++ serialization.py
@@ -2,8 +2,32 @@ import csv
 import os
 import sklearn as sks
 import sys
+import pandas as pd
+import clustering
 
-def execute(N: int, M: int, L: int):
+def import_csv(directory_path, file=None, all=False):
+    if file:
+        df = pd.read_csv(f"{directory_path}/{file}", lineterminator='\n')
+    else:
+        for filename in os.listdir(directory_path):
+            df = pd.read_csv(f"{directory_path}/{filename}", lineterminator='\n')
+    return df
 
+def execute(directory_path: str, N: int, M: int, L: int):
+    if L >= M:
+        df = import_csv(directory_path)
+        print(len(df))
+        for i in range(N):
+            for i in range(M, L + 1):
+                print(i)
+        return None
+    else:
+        raise ValueError
 
 if __name__ == "__main__":
+    directory_path = sys.argv[1]
+    N = sys.argv[2]
+    M = sys.argv[3]
+    L = sys.argv[4]
+
+    execute(directory_path, int(N), int(M), int(L))
