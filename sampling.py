de513783422ce05a6d6af03edcd2d5d2db30321a
Miao-Long <myau.christopher@gmail.com>
2022-11-15 00:34:55 -0800

    Add random state to sampling, create serialization module

--- sampling.py
+++ sampling.py
@@ -1,6 +1,6 @@
 import csv
-import nltk
 import os
+import sklearn as sks
 import sys
 import pandas as pd
 
@@ -20,16 +20,16 @@ def csv_reader(directory_path:str) -> dict:
             except Exception as e: print(e)
     return [df_dict, x/c]
 
-def weighted_sample(d:dict, weight:float):
+def weighted_sample(d:dict, weight:float, random_state:int=None):
     df_list = []
     for key in d.keys():
-        df_list.append(d[key].sample(int(len(d[key]) * weight)))
+        df_list.append(d[key].sample(int(len(d[key]) * weight),random_state=random_state))
     return pd.concat(df_list)
 
-def execute(directory_path: str, export_path: str, sample_size: int, runs: int):
+def execute(directory_path: str, export_path: str, sample_size: int=100, runs: int=1, random_state:int=None):
     [data_full, avg_len] = csv_reader(directory_path)
     for i in range(runs):
-        data_sampled_combined = weighted_sample(data_full, float(int(sample_size)/avg_len))
+        data_sampled_combined = weighted_sample(data_full, float(sample_size/avg_len), random_state)
         data_sampled_combined.to_csv(f"{export_path}/UkraineWarSampled_{i}.csv")
 
 if __name__ == "__main__":
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     sample_size = sys.argv[3]
     runs = sys.argv[4]
 
-    execute(directory_path, export_path, sample_size, int(runs))
+    execute(directory_path, export_path, int(sample_size), int(runs))
 
 
 
---
+++ serialization.py
@@ -0,0 +1,9 @@
+import csv
+import os
+import sklearn as sks
+import sys
+
+def execute(N: int, M: int, L: int):
+
+
+if __name__ == "__main__":
