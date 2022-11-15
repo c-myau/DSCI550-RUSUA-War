0638247270c79166dc32a4efbf6fc2f4920e1dc0
Miao-Long <myau.christopher@gmail.com>
2022-11-14 23:24:23 -0800

    Adds parameter definitions

--- sampling.py
+++ sampling.py
@@ -20,13 +20,13 @@ def csv_reader(directory_path:str) -> dict:
             except Exception as e: print(e)
     return [df_dict, x/c]
 
-def weighted_sample(d, weight):
+def weighted_sample(d:dict, weight:float):
     df_list = []
     for key in d.keys():
         df_list.append(d[key].sample(int(len(d[key]) * weight)))
     return pd.concat(df_list)
 
-def execute(directory_path, export_path, sample_size, runs):
+def execute(directory_path: str, export_path: str, sample_size: int, runs: int):
     [data_full, avg_len] = csv_reader(directory_path)
     for i in range(runs):
         data_sampled_combined = weighted_sample(data_full, float(int(sample_size)/avg_len))
